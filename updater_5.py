7bf95d5e25582c4bd0e11ad1dbab935b108ff447
TxominBasterraChang <120520985+TxominBasterraChang@users.noreply.github.com>
2023-09-19 10:27:48 +0200

    Update updater_5.py

--- updater_5.py
+++ updater_5.py
@@ -19,7 +19,7 @@ PATH_METADATA = "ABC"
 BASELINK_DATASHOP = "XYZ"
 
 PROVIDER = "Canton Zurich"
-SHOP_METADATA_LINK = "https://sfbg.de/lhm/lhm_test.json"
+SHOP_METADATA_LINK = "https://sfbg.de/lhm/lhm_test_3.json"
 SHOP_ABBR = "ktzh"
 
 GITHUB_ACCOUNT = "TxominBasterraChang"
