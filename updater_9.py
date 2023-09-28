cf805bc18ca5fa4e3a73f3ec28b731d87e19504d
TxominBasterraChang <120520985+TxominBasterraChang@users.noreply.github.com>
2023-09-28 22:56:07 +0200

    Update updater_9.py

--- updater_9.py
+++ updater_9.py
@@ -19,7 +19,7 @@ PATH_METADATA = "ABC"
 BASELINK_DATASHOP = "https://opendata.muenchen.de/dataset/"
 
 PROVIDER = "Landeshauptstadt München"
-SHOP_METADATA_LINK = "https://sfbg.de/lhm/lhm_test_24.json"
+SHOP_METADATA_LINK = "https://sfbg.de/lhm/lhm_test_23.json"
 SHOP_ABBR = "ktzh"
 
 GITHUB_ACCOUNT = "TxominBasterraChang"
